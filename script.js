bf019c4a744a08538be1124f96425c1a263d7278
Abir18 <rifatabir21@gmail.com>
2022-09-19 17:49:47 +0600

    Layout added from JS

--- script.js
+++ script.js
@@ -1,62 +1,82 @@
-const userId = document.querySelector(".editId");
-const userName = document.querySelector(".editName");
-const userEmail = document.querySelector(".editEmail");
-const userAddress = document.querySelector(".editAddress");
-const list = document.querySelector("table");
-const update = document.querySelector(".update");
-
 // Users Information in Table
 // const tableUserId = document.querySelector(`.tableUserId`);
 // const tableUserEmail = document.querySelector(".tableUserEmail");
 // const tableUserAddress = document.querySelector(".tableUserAddress");
 
-let id = 1;
+const userId = document.querySelector(".editId")
+const userName = document.querySelector(".editName")
+const userEmail = document.querySelector(".editEmail")
+const userAddress = document.querySelector(".editAddress")
+const list = document.querySelector("table")
+const update = document.querySelector(".update")
 
-let users = [];
+update.disabled = true
+update.style.background = "#f3f3f3"
+update.style.color = "#ddd"
+
+userName.addEventListener("keyup", e => {
+    update.disabled = false
+    update.style.background = "#fff"
+    update.style.color = "#000"
+})
+userEmail.addEventListener("keyup", e => {
+    update.disabled = false
+    update.style.background = "#fff"
+    update.style.color = "#000"
+})
+userAddress.addEventListener("keyup", e => {
+    update.disabled = false
+    update.style.background = "#fff"
+    update.style.color = "#000"
+})
+
+let id = 1
+
+let users = []
 
 const fetchData = async () => {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
-  const data = await response.json();
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`)
+    const data = await response.json()
 
-  users = [...data];
-  // console.log(users, "users");
-};
-fetchData();
+    users = [...data]
+    // console.log(users, "users");
+}
+fetchData()
 
-const tableBody = document.querySelector("tbody");
-const rowElement = document.createElement("tr");
+const tableBody = document.querySelector("tbody")
+const rowElement = document.createElement("tr")
 
-const editUserName = document.querySelector(".editName");
-const editUserEmail = document.querySelector(".editEmail");
-const editUserAddress = document.querySelector(".editAddress");
-const editUserId = document.querySelector(".editId");
+const editUserName = document.querySelector(".editName")
+const editUserEmail = document.querySelector(".editEmail")
+const editUserAddress = document.querySelector(".editAddress")
+const editUserId = document.querySelector(".editId")
 
 const loadIntoTable = async (url, table) => {
-  const response = await fetch(url);
-  //   const { id, name, email } = await response.json();
-  const dataa = await response.json();
+    const response = await fetch(url)
+    //   const { id, name, email } = await response.json();
+    const dataa = await response.json()
 
-  editUserId.value = users[0].id;
-  editUserName.value = users[0].name;
-  editUserEmail.value = users[0].email;
-  editUserAddress.value = users[0].address.city;
+    editUserId.value = users[0].id
+    editUserName.value = users[0].name
+    editUserEmail.value = users[0].email
+    editUserAddress.value = users[0].address.city
 
-  let tableData = "";
-  users.map((user) => {
-    tableData += `
+    let tableData = ""
+    users.map(user => {
+        tableData += `
     <tr data-id=${user.id}>
     <td class="tableUserId-${user.id}">${user.id}</td>
     <td class="tableUserName-${user.id}">${user.name}</td>
     <td class="tableUserEmail-${user.id}">${user.email}</td>
     <td class="tableUserAddress-${user.id}">${
-      user?.address?.city || "Germany"
-    }</td>
+            user?.address?.city || "Germany"
+        }</td>
 
-    <td>
+    <td class="button-layout">
     ${
-      /* Comment in Template String */
-      /* <button id="edit-user" onclick="editUserInfo(${d.id})">Edit</button> */ ""
-      /* <button class="edit" id="edit-user">Edit</button> */
+        /* Comment in Template String */
+        /* <button id="edit-user" onclick="editUserInfo(${d.id})">Edit</button> */ ""
+        /* <button class="edit" id="edit-user">Edit</button> */
     }
     
     <button class="edit" id="edit-user-${user.id}">Edit</button>
@@ -65,182 +85,187 @@ const loadIntoTable = async (url, table) => {
     </td>
 
     </tr>
-    `;
-    // console.log(tableData);
-  });
-  tableBody.innerHTML = tableData;
-  editList();
-};
+    `
+        // console.log(tableData);
+    })
+    tableBody.innerHTML = tableData
+    editList()
+}
 
 const resetForm = () => {
-  // e.preventDefault();
-  document.querySelector(".editName").value = document.querySelector(
-    `.tableUserName-${id}`
-  ).innerText;
-  document.querySelector(".editEmail").value = document.querySelector(
-    `.tableUserEmail-${id}`
-  ).innerText;
-  document.querySelector(".editAddress").value = document.querySelector(
-    `.tableUserAddress-${id}`
-  ).innerText;
+    console.log(id, "resetId")
+    // e.preventDefault();
+    document.querySelector(".editName").value = document.querySelector(
+        `.tableUserName-${id}`
+    ).innerText
+    document.querySelector(".editEmail").value = document.querySelector(
+        `.tableUserEmail-${id}`
+    ).innerText
+    document.querySelector(".editAddress").value = document.querySelector(
+        `.tableUserAddress-${id}`
+    ).innerText
 
-  document
-    .querySelectorAll(".form-control")
-    .forEach((element) => (element.className = "form-control"));
+    document
+        .querySelectorAll(".form-control")
+        .forEach(element => (element.className = "form-control"))
 
-  // document.querySelector(".editEmail").value = tableUserEmail;
-  // document.querySelector(".editAddress").value = tableUserAddress;
-};
+    // document.querySelector(".editEmail").value = tableUserEmail;
+    // document.querySelector(".editAddress").value = tableUserAddress;
+}
 
 const editList = () => {
-  // console.log(update);
+    // console.log(update);
 
-  list.addEventListener("click", async (e) => {
-    const url = `https://jsonplaceholder.typicode.com/users`;
-    id = e.target.parentElement.parentElement.dataset.id;
+    list.addEventListener("click", async e => {
+        const url = `https://jsonplaceholder.typicode.com/users`
+        id = e.target.parentElement.parentElement.dataset.id
 
-    // console.log(e.target.id, "e.target.id");
-    // console.log(id, "dataset.id");
+        // console.log(e.target.id, "e.target.id");
+        // console.log(id, "dataset.id");
 
-    let editButtonIsPressed = e.target.id === `edit-user-${id}`;
-    let deleteButtonIsPressed = e.target.id === `delete-user-${id}`;
-    let updateButtonIsPressed = e.target.parentElement.className === "update";
+        let editButtonIsPressed = e.target.id === `edit-user-${id}`
+        let deleteButtonIsPressed = e.target.id === `delete-user-${id}`
+        let updateButtonIsPressed =
+            e.target.parentElement.className === "update"
 
-    // console.log(e.target.parentElement, "parent");
+        // console.log(e.target.parentElement, "parent");
 
-    // update.addEventListener("click", () => {
-    //   console.log("update button pressed");
-    // });
+        // update.addEventListener("click", () => {
+        //   console.log("update button pressed");
+        // });
 
-    if (editButtonIsPressed) {
-      // console.log("User Edited");
-      // console.log(id);
+        if (editButtonIsPressed) {
+            // console.log("User Edited");
+            // console.log(id);
 
-      const tableUserId = document.querySelector(
-        `.tableUserId-${id}`
-      ).textContent;
-      const tableUserName = document.querySelector(
-        `.tableUserName-${id}`
-      ).textContent;
-      const tableUserEmail = document.querySelector(
-        `.tableUserEmail-${id}`
-      ).textContent;
-      const tableUserAddress = document.querySelector(
-        `.tableUserAddress-${id}`
-      ).textContent;
-      console.log(tableUserName, "tableUserName");
+            const tableUserId = document.querySelector(
+                `.tableUserId-${id}`
+            ).textContent
+            const tableUserName = document.querySelector(
+                `.tableUserName-${id}`
+            ).textContent
+            const tableUserEmail = document.querySelector(
+                `.tableUserEmail-${id}`
+            ).textContent
+            const tableUserAddress = document.querySelector(
+                `.tableUserAddress-${id}`
+            ).textContent
+            // console.log(tableUserName, "tableUserName")
 
-      // const response = await fetch(
-      //   `https://jsonplaceholder.typicode.com/users/${id}`
-      // );
-      // const data = await response.json();
-      // const { name, email, address } = await response.json();
+            // const response = await fetch(
+            //   `https://jsonplaceholder.typicode.com/users/${id}`
+            // );
+            // const data = await response.json();
+            // const { name, email, address } = await response.json();
 
-      // console.log(name, email, "singleUser");
-      console.log(userId, "userId");
+            // console.log(name, email, "singleUser");
+            // console.log(userId, "userId")
 
-      const editUserId = document.querySelector(".editId");
-      // console.log(editUserId, "ew");
-      userId.value = id;
-      userName.value = tableUserName;
-      userEmail.value = tableUserEmail;
-      // userEmail.value = email;
-      userAddress.value = tableUserAddress || "404";
-      // userAddress.value = address?.city || "404 Not Found";
-    }
+            const editUserId = document.querySelector(".editId")
+            // console.log(editUserId, "ew");
+            userId.value = id
+            userName.value = tableUserName
+            userEmail.value = tableUserEmail
+            // userEmail.value = email;
+            userAddress.value = tableUserAddress || "404"
+            // userAddress.value = address?.city || "404 Not Found";
+        }
 
-    if (updateButtonIsPressed) {
-      console.log("update button pressed");
-    }
+        if (updateButtonIsPressed) {
+            console.log("update button pressed")
+        }
 
-    if (deleteButtonIsPressed) {
-      if (confirm("Are you sure to delete this user?")) {
-        const deleteId = document.querySelector(`#delete-user-${id}`);
-        // const deleteId = document.getElementById("delete-user-" + id);
-        // console.log(deleteId, "idp");
-        // console.log(id, "oo");
+        if (deleteButtonIsPressed) {
+            if (confirm("Are you sure to delete this user?")) {
+                const deleteId = document.querySelector(`#delete-user-${id}`)
+                // const deleteId = document.getElementById("delete-user-" + id);
+                // console.log(deleteId, "idp");
+                // console.log(id, "oo");
 
-        // console.log(id, 'id');
+                // console.log(id, 'id');
 
-        const filteredUsers = users.filter((user) => user.id != id);
+                const filteredUsers = users.filter(user => user.id != id)
 
-        users = [...filteredUsers];
-        // console.log(id, "id");
+                users = [...filteredUsers]
+                // console.log(id, "id");
 
-        // console.log(filteredUsers, "filteredUsers");
+                // console.log(filteredUsers, "filteredUsers");
 
-        deleteId.parentElement.parentElement.remove();
-        // resetForm();
+                deleteId.parentElement.parentElement.remove()
+                // resetForm();
 
-        editUserId.value = filteredUsers[0].id;
-        editUserName.value = filteredUsers[0].name;
-        editUserEmail.value = filteredUsers[0].email;
-        editUserAddress.value = filteredUsers[0].address.city;
-      }
-    }
-  });
-};
-
-update.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  const validName = editedUsernameValidation();
-  const validEmail = editedEmailValidation();
-  const validAddress = editedAddressValidation();
-
-  if (!validName || !validEmail || !validAddress) {
-    // alert("Please Insert Information Correctly");
-    return;
-  }
-
-  if (confirm("Are you sure to update this user?")) {
-    document
-      .querySelectorAll(".form-control")
-      .forEach((element) => (element.className = "form-control"));
-
-    fetch(`https://jsonplaceholder.typicode.com/users/${userId.value}`, {
-      method: "PATCH",
-      body: JSON.stringify({
-        id: userId.value,
-        name: userName.value,
-        email: userEmail.value,
-        address: userAddress.value,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
+                editUserId.value = filteredUsers[0].id
+                editUserName.value = filteredUsers[0].name
+                editUserEmail.value = filteredUsers[0].email
+                editUserAddress.value = filteredUsers[0].address.city
+            }
+        }
     })
-      .then((response) => response.json())
-      .then((data) => {
-        const datasetId = document.querySelector("table tbody tr")?.dataset.id;
+}
 
-        if (userId.value === data.id) {
-          // console.log("ID matched");
+update.addEventListener("click", e => {
+    e.preventDefault()
 
-          const trow = tableBody.querySelector(`tr `);
+    const validName = editedUsernameValidation()
+    const validEmail = editedEmailValidation()
+    const validAddress = editedAddressValidation()
 
-          let singRow = document.querySelector(`[data-id='${data.id}']`);
-          // console.log(singRow, "singRow");
+    if (!validName || !validEmail || !validAddress) {
+        // alert("Please Insert Information Correctly");
+        return
+    }
 
-          // console.log(data, "updataaa");
+    if (confirm("Are you sure to update this user?")) {
+        document
+            .querySelectorAll(".form-control")
+            .forEach(element => (element.className = "form-control"))
 
-          singRow.innerHTML = `
+        fetch(`https://jsonplaceholder.typicode.com/users/${userId.value}`, {
+            method: "PATCH",
+            body: JSON.stringify({
+                id: userId.value,
+                name: userName.value,
+                email: userEmail.value,
+                address: userAddress.value,
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                const datasetId =
+                    document.querySelector("table tbody tr")?.dataset.id
+
+                if (userId.value === data.id) {
+                    // console.log("ID matched");
+
+                    const trow = tableBody.querySelector(`tr `)
+
+                    let singRow = document.querySelector(
+                        `[data-id='${data.id}']`
+                    )
+                    // console.log(singRow, "singRow");
+
+                    // console.log(data, "updataaa");
+
+                    singRow.innerHTML = `
           <tr data-id=${data.id}>
           <td  class="tableUserId-${data.id}">${data.id}</td>
           <td class="tableUserName-${data.id}">${
-            data?.name || "Leanne Graham"
-          }</td>
+                        data?.name || "Leanne Graham"
+                    }</td>
           <td class="tableUserEmail-${data.id}">${
-            data?.email || "abc@abc.com"
-          }</td>
+                        data?.email || "abc@abc.com"
+                    }</td>
           <td class="tableUserAddress-${data.id}">${
-            data?.address || "Germany"
-          }</td>
+                        data?.address || "Germany"
+                    }</td>
           
           <td>
           ${
-            /* Comment in Template String */
-            /* <button id="edit-user" onclick="editUserInfo(${d.id})">Edit</button> */ ""
+              /* Comment in Template String */
+              /* <button id="edit-user" onclick="editUserInfo(${d.id})">Edit</button> */ ""
           }
           
           <button class="edit" id="edit-user-${data.id}" >Edit</button>
@@ -248,13 +273,13 @@ update.addEventListener("click", (e) => {
           </td>
           
           </tr>
-          `;
-        }
-      });
-  }
-});
+          `
+                }
+            })
+    }
+})
 
 loadIntoTable(
-  "https://jsonplaceholder.typicode.com/users",
-  document.querySelector("table")
-);
+    "https://jsonplaceholder.typicode.com/users",
+    document.querySelector("table")
+)
---
+++ template.js
@@ -0,0 +1,110 @@
+const root = document.querySelector("#root")
+const template = document.createElement("main")
+
+template.innerHTML = `
+	<div id="wrapper">
+        <table align="center" class="table" id="data_table">
+            <thead>
+                <tr>
+                    <th colspan="4" style="text-align: left">User List</th>
+                    <th>
+                        <button id="myBtn" class="new-btn"> + New</button>
+                    </th>
+                </tr>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Address</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+
+        <div class="vertical"></div>
+
+        <section class="edit-section">
+            <div>
+                <h2>Edit details</h2>
+                <hr />
+            </div>
+            <form id="edit-form" novalidate>
+                <div>
+                    <input class="editId" type="hidden" />
+                </div>
+                <div class="form-control">
+                    <label for="name">Name:</label>
+
+                    <input id="name" type="text" class="editName" />
+
+                    <small class="errName">Error message</small>
+                    <i class="fas fa-check-circle"></i>
+                    <i class="fas fa-exclamation-circle"></i>
+                </div>
+                <div class="form-control">
+                    <label for="email">Email:</label>
+                    <input id="email" type="email" class="editEmail" />
+                    <small class="errName">Error message</small>
+                    <i class="fas fa-check-circle"></i>
+                    <i class="fas fa-exclamation-circle"></i>
+                </div>
+                <div class="form-control">
+                    <label for="address">Address:</label>
+                    <textarea name="" id="address" cols="30" rows="10" class="editAddress"></textarea>
+                    <small class="errName">Error message</small>
+                    <i class="fas fa-check-circle"></i>
+                    <i class="fas fa-exclamation-circle"></i>
+                </div>
+                <button class="update">Update</button>
+                <button class="reset" onclick="resetForm()">Reset</button>
+            </form>
+        </section>
+        <div class="popup">
+            <div id="myModal" class="modal">
+                <div class="modal-content">
+                    <span class="close">&times;</span>
+                    <div class="modal-info">
+                        <div class="modal-header">
+                            <h2>New User</h2>
+                            <hr class="new-user-line" />
+                        </div>
+
+                        <form id="form" novalidate>
+                            <div>
+                                <div class="form-control">
+                                    <label for="username">Name:</label>
+                                    <input type="text" id="username" class="newUserName" />
+                                    <small class="errName">Error message</small>
+                                    <i class="fas fa-check-circle"></i>
+                                    <i class="fas fa-exclamation-circle"></i>
+                                </div>
+                                <div class="form-control">
+                                    <label for="useremail">Email:</label>
+                                    <input id="useremail" type="email" class="newUserEmail" required />
+                                    <i class="fas fa-check-circle"></i>
+                                    <i class="fas fa-exclamation-circle"></i>
+                                    <small class="errName">Error message</small>
+                                </div>
+                                <div class="form-control">
+                                    <label for="useraddress">Address:</label>
+                                    <textarea name="" id="useraddress" cols="30" rows="7"
+                                        class="newUserAddress"></textarea>
+                                    <i class="fas fa-check-circle"></i>
+                                    <i class="fas fa-exclamation-circle"></i>
+                                    <small class="errName">Error message</small>
+                                </div>
+                                <div class="modal-footer">
+                                    <button class="submit" onclick="addNewUser(this)">Save</button>
+                                    <button id="close-modal">Cancel</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</div>
+`
+
+root.appendChild(template)
